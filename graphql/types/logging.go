61c997fd4b2bd233e6cfb2709ba1ddfa0ac7145e
Noel <cutie@floofy.dev>
2021-07-09 01:59:41 +0100

    fix indents on logging

--- graphql/types/logging.go
+++ graphql/types/logging.go
@@ -4,6 +4,6 @@ type Logging struct {
 	IgnoreChannels []string `json:"ignoreChannels"`
 	IgnoreUsers    []string `json:"ignoreUsers"`
 	ChannelID      *string  `json:"channelID"`
-	Enabled 	     bool     `json:"enabled"`
+	Enabled        bool     `json:"enabled"`
 	Events         []string `json:"events"`
 }
